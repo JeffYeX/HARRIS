220daead76fcd807313eee1581d6a4b98dd3f90d
Rorschach <jeffye1992@gmail.com>
2018-05-07 00:38:39 +1200

    Add the main logic for HarrisDetector. WIP: Filter max suppression matrix and display mark on image.

--- HARRIS/HarrisDetector.cs
+++ HARRIS/HarrisDetector.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,35 +10,46 @@ namespace HARRIS
 {
     class HarrisDetector
     {
-        private float k = 0.04f;
+        private float k = 0.25f;
         private float threshold = 20000f;
+        private float percentage = 0.1f;
 
-        // Non-maximum suppression parameters
-        private int r = 3;
+        private int maximaSuppressionDimension = 10;
 
         // Gaussian smoothing parameters
-        private double sigma = 1.2;
-        private float[] kernel;
-        private int size = 7;
+        private int size = 3;
+
+        private float[,] m_harrisResponses;
 
         #region Constructors
 
         /// <summary>
         ///   Initializes a new instance of the <see cref="HarrisCornersDetector"/> class.
         /// </summary>
-        public HarrisDetector()
+        public HarrisDetector(Image image)
         {
-            initialize(k, threshold, sigma, r, size);
+            Initialize(k, threshold);
+            //var width = image.Width;
+            //var height = image.Height;
+            //var srcStride = _grayImage.Stride;
+            //var srcOffset = srcStride - width;
+            //// 1. Calculate partial differences
+            //var diffx = new float[height, width];
+            //var diffy = new float[height, width];
+            //var diffxy = new float[height, width];
+            var resultList = ComputeDerivatives(image);
+            var mDerivatives = ApplyGaussToDerivatives(resultList, size);
+            var harrisResponses = ComputeHarrisResponses(k, mDerivatives);
+            m_harrisResponses = harrisResponses;
         }
 
-
-        private void initialize(float k, float threshold, double sigma, int suppression, int size)
+        private void Initialize(float k, float threshold)
         {
             this.threshold = threshold;
             this.k = k;
-            this.r = suppression;
-            this.sigma = sigma;
-            this.size = size;
+            //this.r = suppression;  , double sigma, int suppression, int size
+            //this.sigma = sigma;
+            //this.size = size;
 
             //createGaussian();
             /*
@@ -49,7 +62,197 @@ namespace HARRIS
             });
             */
         }
-
         #endregion
+
+        private List<float[,]> ComputeDerivatives(Image image)
+        {
+            
+            var bitmap = new Bitmap(image);
+            var resultList = new List<float[,]> { };
+            var vercerticalSobel = new float[bitmap.Height - 2, bitmap.Width];
+            //Console.WriteLine(bitmap.Width + "**" + bitmap.Height);
+            for (var r = 1; r < bitmap.Height - 1; r++)
+            {
+                //Console.WriteLine("r:" + r);
+                for (var c = 0; c < bitmap.Width; c++)
+                {
+                    //Console.WriteLine(" c:" + c);
+                    float a1 = bitmap.GetPixel(c, r - 1).ToArgb();
+                    //Console.WriteLine(" test");
+                    float a2 = bitmap.GetPixel(c, r).ToArgb();
+                    float a3 = bitmap.GetPixel(c, r + 1).ToArgb();
+
+                    vercerticalSobel[r - 1, c] = a1 + a2 + a2 + a3;
+                }
+            }
+
+            var horizontalSobel = new float[bitmap.Height, bitmap.Width - 2];
+            //Console.WriteLine(bitmap.Width + "**" + bitmap.Height);
+            for (var r = 0; r < bitmap.Height; r++)
+            {
+                //Console.WriteLine("r:" + r);
+                for (var c = 1; c < bitmap.Width - 1; c++)
+                {
+                    //Console.WriteLine(" c:" + c);
+                    float a1 = bitmap.GetPixel(c - 1, r).ToArgb();
+                    //Console.WriteLine(" test");
+                    float a2 = bitmap.GetPixel(c, r).ToArgb();
+                    float a3 = bitmap.GetPixel(c + 1, r).ToArgb();
+
+                    horizontalSobel[r, c - 1] = a1 + a2 + a2 + a3;
+                }
+            }
+
+            var dx = new float[bitmap.Height - 2, bitmap.Width - 2];
+            var dy = new float[bitmap.Height - 2, bitmap.Width - 2];
+            var dxy = new float[bitmap.Height - 2, bitmap.Width - 2];
+
+            for (int r = 0; r < bitmap.Height - 2; r++)
+            {
+                for (int c = 0; c < bitmap.Width - 2; c++)
+                {
+                    dx[r, c] = horizontalSobel[r, c] - horizontalSobel[r + 2, c];
+                    dy[r, c] = vercerticalSobel[r, c] - vercerticalSobel[r, c + 2];
+                    dxy[r, c] = dx[r, c] * dy[r, c];
+                }
+            }
+
+            resultList.Add(dx);
+            resultList.Add(dy);
+            resultList.Add(dxy);
+
+            return resultList;
+        }
+
+        private List<float[,]> ApplyGaussToDerivatives(List<float[,]> dMats, int size)
+        {
+            if (size == 0)
+            {
+                return dMats;
+            }
+            var resultList = new List<float[,]> { };
+
+            resultList.Add(GaussFilter(dMats[0], size));
+            resultList.Add(GaussFilter(dMats[1], size));
+            resultList.Add(GaussFilter(dMats[2], size));
+
+            return resultList;
+        }
+
+        private float[,] GaussFilter(float[,] dMats, int size)
+        {
+            var gaussResult = new float[dMats.GetLength(0) - size * 2, dMats.GetLength(1) - size * 2];
+            var verticalGauss = new float[dMats.GetLength(0) - size * 2, dMats.GetLength(1) - size * 2];
+            for (int r = size; r < dMats.GetLength(0) - size; r++)
+            {
+                for (int c = size; c < dMats.GetLength(1) - size; c++)
+                {
+                    var res = (float)0.0;
+
+                    for (int x = -size; x <= size; x++)
+                    {
+                        var m = 1 / Math.Sqrt(2 * Math.PI) * Math.Exp(-0.5 * x * x);
+
+                        res += (float)(m * dMats[r - size, c - size]);
+                    }
+                    verticalGauss[r - size, c - size] = res;
+                }
+            }
+
+            for (int r = size; r < dMats.GetLength(0) - size; r++)
+            {
+                for (int c = size; c < dMats.GetLength(1) - size; c++)
+                {
+                    var res = (float)0.0;
+
+                    for (int x = -size; x <= size; x++)
+                    {
+                        var m = 1 / Math.Sqrt(2 * Math.PI) * Math.Exp(-0.5 * x * x);
+
+                        res += (float)(m * verticalGauss[r - size, c - size]);
+                    }
+                    gaussResult[r - size, c - size] = res;
+                }
+            }
+
+            return gaussResult;
+        }
+
+        private float[,] ComputeHarrisResponses(float k, List<float[,]> mDerivatives)
+        {
+            var result = new float[mDerivatives[1].GetLength(0), mDerivatives[0].GetLength(1)];
+            for (int r = 0; r < mDerivatives[1].GetLength(0); r++)
+            {
+                for (int c = 0; c < mDerivatives[1].GetLength(1); c++)
+                {
+                    var a11 = mDerivatives[0][r, c] * mDerivatives[0][r, c];
+                    var a22 = mDerivatives[1][r, c] * mDerivatives[1][r, c];
+                    var a21 = mDerivatives[0][r, c] * mDerivatives[1][r, c];
+                    var a12 = mDerivatives[0][r, c] * mDerivatives[1][r, c];
+
+                    var det = a11 * a22 - a12 * a21;
+                    var trace = a11 + a22;
+
+                    //var M = Math.Abs(det - k * trace * trace);
+
+                    
+                    result[r, c] = Math.Abs(det - k * trace * trace);
+                }
+            }
+
+            return result;
+        }
+
+        public float[,] ReturnHarris()
+        {
+            return m_harrisResponses;
+        }
+
+        public List<Point> GetMaximaPoints(float percentage, float size, int maximaSuppressionDimension)
+        {
+            var topPoint = new List<Point> { };
+            var maximaSuppressionMat = new int[m_harrisResponses.GetLength(0), m_harrisResponses.GetLength(1)];
+
+
+            
+
+
+            //Sort<float>(ToJagged(m_harrisResponses), 2);
+            Console.WriteLine("sdsd");
+            for (int r = 0; r < m_harrisResponses.GetLength(0); r++)
+            {
+                for (int c = 0; c < m_harrisResponses.GetLength(1); c++)
+                {
+                    var p = new Point(c, r);
+                    
+                }
+            }
+
+
+            return topPoint;
+        }
+
+        private void Sort<T>(T[][] data, int col)
+        {
+            Comparer<T> comparer = Comparer<T>.Default;
+            Array.Sort<T[]>(data, (x, y) => comparer.Compare(x[col], y[col]));
+        }
+
+        private float[][] ToJagged(float[,] array)
+        {
+            int height = array.GetLength(0), width = array.GetLength(1);
+            float[][] jagged = new float[height][];
+
+            for (int i = 0; i < height; i++)
+            {
+                float[] row = new float[width];
+                for (int j = 0; j < width; j++)
+                {
+                    row[j] = array[i, j];
+                }
+                jagged[i] = row;
+            }
+            return jagged;
+        }
     }
 }
--- HARRIS/MainForm.cs
+++ HARRIS/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,7 +39,8 @@ namespace HARRIS
                 {
                     //Image blah = new
                     var inputImage = new Bitmap(ofd.FileName);
-                    _grayImage = ToolStripRenderer.CreateDisabledImage(inputImage);
+                    _grayImage = MakeGrayscale3(inputImage);
+                    //_grayImage = ToolStripRenderer.CreateDisabledImage(inputImage);
                     pictureBox1.Image = _grayImage;
                 }
             }
@@ -52,17 +54,92 @@ namespace HARRIS
         private void button1_Click(object sender, EventArgs e)
         {
             // get source image size
-            var width = _grayImage.Width;
-            var height = _grayImage.Height;
+            //var width = _grayImage.Width;
+            //var height = _grayImage.Height;
             //var srcStride = _grayImage.Stride;
             //var srcOffset = srcStride - width;
 
             // 1. Calculate partial differences
-            var diffx = new float[height, width];
-            var diffy = new float[height, width];
-            var diffxy = new float[height, width];
+            //var diffx = new float[height, width];
+            //var diffy = new float[height, width];
+            //var diffxy = new float[height, width];
+            float k = 0.25f;
+            float threshold = 20000f;
+            float percentage = 0.1f;
 
-            
+            int maximaSuppressionDimension = 10;
+            int size = 3;
+
+            var harris = new HarrisDetector(_grayImage);
+            var blah = harris.ReturnHarris();
+            Graphics g = Graphics.FromImage(_grayImage);
+            for (int i = 0; i < blah.GetLength(0); i++)
+            {
+                for (int c = 0; c < blah.GetLength(1); c++)
+                {
+                    if (blah[i,c] > 1.0 * Math.Pow(10,21))
+                    {
+                        
+                        PaintCross(g, new Point(i, c));
+                    }
+                }
+            }
+            //var resPts = harris.GetMaximaPoints(percentage, size, maximaSuppressionDimension);
+
+
+
+        }
+
+        public static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+              {
+                 new float[] {.3f, .3f, .3f, 0, 0},
+                 new float[] {.59f, .59f, .59f, 0, 0},
+                 new float[] {.11f, .11f, .11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {0, 0, 0, 0, 1}
+              });
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        private void PaintCross(Graphics g, Point loc)
+        {
+            //Half length of the line.
+            const int HALF_LEN = 5;
+
+            //Draw horizontal line.
+
+            Point p1 = new Point(loc.X - HALF_LEN, loc.Y);
+
+            Point p2 = new Point(loc.X + HALF_LEN, loc.Y);
+
+            g.DrawLine(Pens.Red, p1, p2);
+
+            //Draw the vertical line.
+
+            p1 = new Point(loc.X, loc.Y - HALF_LEN);
+
+            p2 = new Point(loc.X, loc.Y + HALF_LEN);
+
+            g.DrawLine(Pens.Red, p1, p2);
+
         }
     }
 }
